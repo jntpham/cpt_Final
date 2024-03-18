5dd4e5ed48d297701c96ee1269359bc006fab72a
Th35py27 <nvallamkondu27@gmail.com>
2024-03-18 12:58:44 -0700

    titanic api finished

--- api/titanic.py
+++ api/titanic.py
@@ -1,41 +1,41 @@
-from flask import Flask, Blueprint, request, jsonify
+from flask import Blueprint, jsonify, Flask, request
 import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
-import json, jwt
-from flask import Blueprint, request, jsonify, current_app, Response
-from flask_restful import Api, Resource # used for REST API building
-from datetime import datetime
-from auth_middleware import token_required
+from flask_restful import Api, Resource
+from sklearn.linear_model import LogisticRegression
 
-
-titanic_api = Blueprint('titanic_api', __name__,
-                   url_prefix='/api/titanic')
-
+app = Flask(__name__)
+titanic_api = Blueprint('titanic_api', __name__, url_prefix='/api/titanic')
 api = Api(titanic_api)
 
+class Predict(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            passenger_data = pd.DataFrame(data, index=[0])
+            
+            # Preprocess the new passenger data
+            passenger_data['sex'] = passenger_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
+            passenger_data['alone'] = passenger_data['alone'].apply(lambda x: 1 if x == True else 0)
+            onehot = enc.transform(passenger_data[['embarked']]).toarray()
+            cols = ['embarked_' + val for val in enc.categories_[0]]
+            passenger_data[cols] = pd.DataFrame(onehot)
+            passenger_data.drop(['embarked'], axis=1, inplace=True)
+            
+            # Predict the survival probability for the new passenger
+            survival_prob = logreg.predict_proba(passenger_data)[:, 1]
+            death_prob = 1 - survival_prob
 
+            return {'death_probability': float(death_prob), 'survival_probability': float(survival_prob)}, 200
+        except Exception as e:
+            return {'error': str(e)}, 400
 
-class TitanicAPI:        
-    class predict(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
-      def post(self):
-        data = request.get_json()
-        passenger_data = pd.DataFrame(data, index=[0])
-        passenger_data['sex'] = passenger_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
-        passenger_data['alone'] = passenger_data['alone'].apply(lambda x: 1 if x == True else 0)
-        onehot = enc.transform(passenger_data[['embarked']]).toarray()
-        cols = ['embarked_' + val for val in enc.categories_[0]]
-        passenger_data[cols] = pd.DataFrame(onehot)
-        passenger_data.drop(['embarked'], axis=1, inplace=True)
-        prediction = dt.predict(passenger_data)[0]
-        return jsonify({'survival_prediction': prediction})
+api.add_resource(Predict, '/predict')
 
-
-api.add_resource(TitanicAPI, '/predict')
-
+# Load Titanic dataset
 titanic_data = sns.load_dataset('titanic')
 
 # Preprocess the data
@@ -53,15 +53,15 @@ titanic_data[cols] = pd.DataFrame(onehot)
 titanic_data.drop(['embarked'], axis=1, inplace=True)
 titanic_data.dropna(inplace=True)
 
-# Split the data into training and testing sets
+# Split the data into features and target
 X = titanic_data.drop('survived', axis=1)
 y = titanic_data['survived']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Train a decision tree classifier
-dt = DecisionTreeClassifier()
-dt.fit(X_train, y_train)
+# Train the logistic regression model
+logreg = LogisticRegression()
+logreg.fit(X, y)
 
+app.register_blueprint(titanic_api)
 
 if __name__ == "__main__":
     app.run(debug=True)
